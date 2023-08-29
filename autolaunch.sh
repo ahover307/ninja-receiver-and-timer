1bc3c95dc016300254bcc5ccc190d1ae9e07b311
Alex Hover <ahover307@gmail.com>
2023-08-29 12:27:48 -0400

    Print lines

--- autolaunch.sh
+++ autolaunch.sh
@@ -7,14 +7,23 @@ cd ~/Ninja-Receiver-and-timer || exit
 # Git pull to update the application
 git pull
 
+echo "Finished pulling"
+
 # Set up the docker containers
 docker compose up -d --build
+
+echo "Finished docker compose"
 
 # Hide the cursor
 unclutter -idle 0.1
 
+# Disable the screen saver
+echo "Unclutter was activated"
+
 # Open the browser to the application
 chromium-browser --kiosk --incognito --disable-session-crashed-bubble --disable-infobars http://localhost/display
+
+echo "And browser was launched"
 
 # Exit the script
 exit 0
