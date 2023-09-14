84e21a3b70b900c98449fad50571b1614911528e
Alex Hover <ahover307@gmail.com>
2023-09-14 15:28:59 -0400

    Network retry

--- autolaunch.sh
+++ autolaunch.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # This script is run at boot to start the application automatically
 
+# Wait for the network to be connected
+max_retries=5
+retries=0
+
+while [ $retries -lt $max_retries ]; do
+    if ping -c 1 google.com &>/dev/null; then
+        # Network is connected, proceed with your script
+        echo "Network is connected. Starting your script."
+        # Add your script logic here
+        break
+    else
+        retries=$((retries+1))
+        echo "Network not connected. Retry $retries/$max_retries."
+        sleep 10
+    fi
+done
+
+if [ $retries -eq $max_retries ]; then
+    echo "Max retries reached. Exiting without running the script."
+fi
+
 # Change directory to the application directory
 cd ~/Ninja-Receiver-and-timer || exit
 
