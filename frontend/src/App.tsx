0817b99dcf54ae4d2607c2092ee753ac790d6577
Alex Hover <ahover307@gmail.com>
2023-10-24 23:30:53 -0400

    Updated landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Display } from "./components/Display.tsx";
-import {createClient, SupabaseClient} from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { anon_key, endpoint } from "./Constants/Env.ts";
 
 export const App = () => {
@@ -11,13 +11,23 @@ export const App = () => {
   useEffect(() => {
     setSupabase(createClient(endpoint, anon_key));
   }, []);
-  
+
   if (!supabase) return <div>Connecting to backend... loading</div>;
 
   const router = createBrowserRouter([
     {
-      path: "/:location", element: <div>Please go to /display/paramount, this page is not yet built</div>,
-      }, {
+      path: "/",
+      element: (
+          <div>Please go to <a href={"/display/paramount"}>/display/paramount</a>, this page is not yet built</div>
+      ),
+    },
+    {
+      path: "/:location",
+      element: (
+        <div>Please go to <a href={"/display/paramount"}>/display/paramount</a>, this page is not yet built</div>
+      ),
+    },
+    {
       path: "/display/:location",
       element: <Display supabase={supabase} />,
     },
